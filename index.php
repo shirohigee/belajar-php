6f781eb0996ebed2ab78178df2304a0293491e8b
shirohigee <faldygosal08@gmail.com>
2022-10-24 12:00:41 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hello World <br>";
+$nama = "Faldy";
+$umur = 20;
+
+echo "Nama Saya <strong>$nama</strong>, Saya berumur $umur tahun. <br>";
+
+$namaAdik = "Slim";
+$umurAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, Adik saya berusia $umurAdik tahun.<br>";
+
+$selisih = $umur - $umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisih tahun.";
